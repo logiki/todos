c3d949121948e9241ca1f0e0311024a166946aab
Jason Weber <jason@logiki.com>
2011-12-07 18:30:09 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todos::Application.config.secret_token = 'b26cae9144e4a3eafacba0a8d5160bab81b9e26fc6bc10692618d81cdc604c8d24a8dcc7f72d5d0ca6283dfc93b236596c9e30832506887cbd7ee258a9e363f2'
